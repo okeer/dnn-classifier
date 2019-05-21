9aadc156b1001ac56d35b317f4005b5a3ea7fa05
Sergey Orlov <serhio.orlovsky@gmail.com>
2019-05-21 23:41:55 +0700

    refactor layer tests

--- models/LayerBase.py
+++ models/LayerBase.py
@@ -10,6 +10,7 @@ class LayerBase(object):
         self.bias = None
         self.pre_activation = None
         self.activation = None
+        self.activation_previous = None
 
     def __if_params_not_initialized(self):
         return (self.weights is None) or (self.bias is None)
@@ -18,11 +19,12 @@ class LayerBase(object):
         self.weights = np.random.randn(self.current_layer_dim, size_of_previous_layer) / np.sqrt(self.current_layer_dim)
         self.bias = np.zeros((size_of_previous_layer, 1))
 
-    def __forward_linear(self, activation_from_previous_layer):
+    def __forward_linear(self, activation_previous):
+        self.activation_previous = activation_previous
         if self.__if_params_not_initialized():
-            self.__init_parameters(activation_from_previous_layer.shape[0])
+            self.__init_parameters(activation_previous.shape[0])
 
-        self.pre_activation = self.weights.dot(activation_from_previous_layer) + self.bias
+        self.pre_activation = self.weights.dot(activation_previous) + self.bias
 
     def forward(self, activation_from_previous_layer):
         self.__forward_linear(activation_from_previous_layer)
--- tests/LayerTest.py
+++ tests/LayerTest.py
@@ -7,40 +7,36 @@ from models.LayerBase import LayerBase
 
 
 class LayerTest(unittest.TestCase):
-    def test_givenNewLayer_whenForward_shouldCalculatePreActivation(self):
-        layer = LayerBase(2, lambda x: x)
-        layer.weights = np.array([[0.5, 0.3], [0.1, 0.2]])
-        layer.bias = np.array([[0.1, 0.2]]).reshape((2, 1))
-        features = np.array([[1, 2, 3], [2, 1, 0.5]])
+    def __init_test_layer(self, cls):
+        self.layer = cls(2, lambda x: x)
+        self.layer.weights = np.array([[0.5, 0.3], [0.1, 0.2]])
+        self.layer.bias = np.array([[0.1, 0.2]]).reshape((2, 1))
+        self.features = np.array([[1, 2, 3], [2, 1, 0.5]])
 
+    def test_givenBaseLayer_whenForward_shouldCalculatePreActivation(self):
+        self.__init_test_layer(LayerBase)
         pre_activation_assrt = np.array([[1.2, 1.4, 1.75], [0.7, 0.6, 0.6]])
 
-        layer._LayerBase__forward_linear(features)
+        self.layer._LayerBase__forward_linear(self.features)
 
-        self.assertEqual(True, isinstance(layer.pre_activation, np.ndarray))
-        np.testing.assert_array_almost_equal(pre_activation_assrt, layer.pre_activation)
+        self.assertEqual(True, isinstance(self.layer.pre_activation, np.ndarray))
+        np.testing.assert_array_almost_equal(pre_activation_assrt, self.layer.pre_activation)
 
-    def test_givenNewLayer_whenForward_shouldCalculateActivation(self):
-        layer = SigmoidLayer(2, lambda x: x)
-        layer.weights = np.array([[0.5, 0.3], [0.1, 0.2]])
-        layer.bias = np.array([[0.1, 0.2]]).reshape((2, 1))
-        features = np.array([[1, 2, 3], [2, 1, 0.5]])
+    def test_givenSigmoidLayer_whenForward_shouldCalculateActivation(self):
+        self.__init_test_layer(SigmoidLayer)
         sigmoid_activation_assert = np.array([[0.76852478, 0.80218389, 0.8519528],
                                               [0.66818777, 0.64565631, 0.64565631]])
 
-        activation = layer.forward(features)
+        activation = self.layer.forward(self.features)
 
         np.testing.assert_array_almost_equal(sigmoid_activation_assert, activation)
 
     def test_givenReluLayer_whenForward_shouldCalculateActivation(self):
-        layer = ReluLayer(2, lambda x: x)
-        layer.weights = np.array([[0.5, 0.3], [0.1, 0.2]])
-        layer.bias = np.array([[0.1, 0.2]]).reshape((2, 1))
-        features = np.array([[1, 2, 3], [2, 1, 0.5]])
+        self.__init_test_layer(ReluLayer)
         relu_activation_assert = np.array([[1, 1, 1],
                                            [1, 1, 1]])
 
-        activation = layer.forward(features)
+        activation = self.layer.forward(self.features)
 
         np.testing.assert_array_almost_equal(relu_activation_assert, activation)
 
