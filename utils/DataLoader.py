a1697f9edf6da8e0721aec357ac17996b53dd803
Sergey Orlov <serhio.orlovsky@gmail.com>
2019-05-21 16:18:33 +0700

    refactor data loading
    * reuse code whenever possible
    * flatten features
    * normalize features

--- utils/DataLoader.py
+++ utils/DataLoader.py
@@ -6,6 +6,7 @@ def load_dataset(filename, features_and_classes_names):
     train_dataset = h5py.File(filename, "r")
 
     features = np.array(train_dataset[features_and_classes_names[0]][:])
+    features = features.reshape(features.shape[0], -1).T/255.
 
     classes = np.array(train_dataset[features_and_classes_names[1]][:])
     classes = classes.reshape((1, classes.shape[0]))
