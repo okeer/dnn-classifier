91c353533b7bbdf42307d0f39a8f7b79daddedd1
Sergey Orlov <serhio.orlovsky@gmail.com>
2019-05-21 23:37:56 +0700

    test: add layre with relu act f

--- tests/LayerTest.py
+++ tests/LayerTest.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
 
+from ReluLayer import ReluLayer
 from SigmoidLayer import SigmoidLayer
 from models.LayerBase import LayerBase
 
@@ -24,12 +25,24 @@ class LayerTest(unittest.TestCase):
         layer.weights = np.array([[0.5, 0.3], [0.1, 0.2]])
         layer.bias = np.array([[0.1, 0.2]]).reshape((2, 1))
         features = np.array([[1, 2, 3], [2, 1, 0.5]])
-        sigmoid_activation_assert = np.array([[0.76852478, 0.80218389, 0.8519528 ],
+        sigmoid_activation_assert = np.array([[0.76852478, 0.80218389, 0.8519528],
                                               [0.66818777, 0.64565631, 0.64565631]])
 
         activation = layer.forward(features)
 
         np.testing.assert_array_almost_equal(sigmoid_activation_assert, activation)
+
+    def test_givenReluLayer_whenForward_shouldCalculateActivation(self):
+        layer = ReluLayer(2, lambda x: x)
+        layer.weights = np.array([[0.5, 0.3], [0.1, 0.2]])
+        layer.bias = np.array([[0.1, 0.2]]).reshape((2, 1))
+        features = np.array([[1, 2, 3], [2, 1, 0.5]])
+        relu_activation_assert = np.array([[1, 1, 1],
+                                           [1, 1, 1]])
+
+        activation = layer.forward(features)
+
+        np.testing.assert_array_almost_equal(relu_activation_assert, activation)
 
 
 if __name__ == '__main__':
