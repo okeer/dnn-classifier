3d09b6a9f27c15b6369c768a8ef4a38fc46fd9ed
Sergey Orlov <serhio.orlovsky@gmail.com>
2019-05-21 16:03:06 +0700

    add dataset loader helper

--- tests/UtilsTest.py
+++ tests/UtilsTest.py
@@ -1,16 +1,16 @@
+import os
 import unittest
-import numpy as np
+
+from utils.DataLoader import *
 
 
 class UtilsTest(unittest.TestCase):
-    def setUp(self) -> None:
-        self.loader = DataLoader()
-
     def test_givenDataset_whenLoad_shouldReturnFeaturesAndClasses(self):
-        features, classes = self.loader.load_dataset()
+        features, classes = load_dataset(os.environ["DATASET_FILE_PATH"],
+                                         (os.environ["DATASET_FEATURES"], os.environ["DATASET_CLASSES"]))
 
-        self.assertEqual(isinstance(features, np.array), True)
-        self.assertEqual(isinstance(classes, np.array), True)
+        self.assertEqual(isinstance(features, np.ndarray), True)
+        self.assertEqual(isinstance(classes, np.ndarray), True)
 
 
 if __name__ == '__main__':
---
+++ utils/DataLoader.py
@@ -0,0 +1,13 @@
+import h5py
+import numpy as np
+
+
+def load_dataset(filename, features_and_classes_names):
+    train_dataset = h5py.File(filename, "r")
+
+    features = np.array(train_dataset[features_and_classes_names[0]][:])
+
+    classes = np.array(train_dataset[features_and_classes_names[1]][:])
+    classes = classes.reshape((1, classes.shape[0]))
+
+    return features, classes
