8ea3de5b618b98460adf018a2b966a1857af09f6
Sergey Orlov <serhio.orlovsky@gmail.com>
2019-05-21 23:14:20 +0700

    refactor Layer impl and test

--- models/LayerBase.py
+++ models/LayerBase.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class Layer(object):
+class LayerBase(object):
     def __init__(self, units_count, activation_func):
         self.current_layer_dim = units_count
         self.activation_func = activation_func
@@ -18,14 +18,14 @@ class Layer(object):
         self.weights = np.random.randn(self.current_layer_dim, size_of_previous_layer) / np.sqrt(self.current_layer_dim)
         self.bias = np.zeros((size_of_previous_layer, 1))
 
-    def forward(self, activation_from_previous_layer):
+    def __forward_linear(self, activation_from_previous_layer):
         if self.__if_params_not_initialized():
             self.__init_parameters(activation_from_previous_layer.shape[0])
 
         self.pre_activation = self.weights.dot(activation_from_previous_layer) + self.bias
-        self.activation = self.activation_func(self.pre_activation)
 
-        return self.activation
+    def forward(self, activation_from_previous_layer):
+        self.__forward_linear(activation_from_previous_layer)
 
     def backward(self):
         pass
--- tests/LayerTest.py
+++ tests/LayerTest.py
@@ -1,22 +1,35 @@
 import unittest
 import numpy as np
 
-from models.Layer import Layer
+from SigmoidLayer import SigmoidLayer
+from models.LayerBase import LayerBase
 
 
 class LayerTest(unittest.TestCase):
     def test_givenNewLayer_whenForward_shouldCalculatePreActivation(self):
-        layer = Layer(2, lambda x: x)
+        layer = LayerBase(2, lambda x: x)
         layer.weights = np.array([[0.5, 0.3], [0.1, 0.2]])
         layer.bias = np.array([[0.1, 0.2]]).reshape((2, 1))
         features = np.array([[1, 2, 3], [2, 1, 0.5]])
 
         pre_activation_assrt = np.array([[1.2, 1.4, 1.75], [0.7, 0.6, 0.6]])
 
-        layer.forward(features)
+        layer._LayerBase__forward_linear(features)
 
         self.assertEqual(True, isinstance(layer.pre_activation, np.ndarray))
         np.testing.assert_array_almost_equal(pre_activation_assrt, layer.pre_activation)
+
+    def test_givenNewLayer_whenForward_shouldCalculateActivation(self):
+        layer = SigmoidLayer(2, lambda x: x)
+        layer.weights = np.array([[0.5, 0.3], [0.1, 0.2]])
+        layer.bias = np.array([[0.1, 0.2]]).reshape((2, 1))
+        features = np.array([[1, 2, 3], [2, 1, 0.5]])
+        sigmoid_activation_assert = np.array([[0.76852478, 0.80218389, 0.8519528 ],
+                                              [0.66818777, 0.64565631, 0.64565631]])
+
+        activation = layer.forward(features)
+
+        np.testing.assert_array_almost_equal(sigmoid_activation_assert, activation)
 
 
 if __name__ == '__main__':
