875354d871a3293f2ed5b3a2fdd18272cbc9e257
Sergey Orlov <serhio.orlovsky@gmail.com>
2019-05-22 17:47:16 +0700

    tests: fix relu test

--- tests/ActivationFuncTest.py
+++ tests/ActivationFuncTest.py
@@ -8,7 +8,7 @@ class ActivationFuncTest(unittest.TestCase):
         np.testing.assert_almost_equal(sigmoid(1), 0.7310585786300049)
 
     def test_relu(self):
-        np.testing.assert_array_almost_equal(np.array([1, 1, 1, 0]), relu(np.array([0.7, 0.6, 0.9, 0.1])))
+        np.testing.assert_array_almost_equal(np.array([0.7, 0.6, 0, 0.1]), relu(np.array([0.7, 0.6, -0.9, 0.1])))
 
 
 if __name__ == '__main__':
