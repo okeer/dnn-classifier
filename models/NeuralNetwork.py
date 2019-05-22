Fix bug which prevented gradient descent from iterating

models/NeuralNetwork.py
@@ -2,10 +2,12 @@ import numpy as np
 
 
 class NeuralNetwork(object):
-    def __init__(self, layers):
+    def __init__(self, layers, learning_rate, iterations):
         self.layers = layers
         self.layers_count = len(self.layers)
         self.propagation_features = None
+        self.learning_rate = learning_rate
+        self.iterations = iterations
 
     def __forward_propagation(self, features):
         self.propagation_features = features
@@ -15,12 +17,14 @@ class NeuralNetwork(object):
 
     def __backward_propagation(self, classes):
         d_activation = (np.divide(classes, self.propagation_features) - np.divide(1 - classes, 1 - self.propagation_features))
-        for index in range(len(self.layers) - 1, 0):
+        for index in reversed(range(len(self.layers) - 1)):
             d_activation = self.layers[index].backward(d_activation)
+            self.layers[index].update(self.learning_rate)
 
     def train(self, features, classes):
-        self.__forward_propagation(features)
-        self.__backward_propagation(classes)
+        for index in range(self.iterations):
+            self.__forward_propagation(features)
+            self.__backward_propagation(classes)
 
     def predict(self, features):
         self.__forward_propagation(features)