ea896d7639f4f767adbcfd283f048c1a3b93bc0e
Sergey Orlov <serhio.orlovsky@gmail.com>
2019-05-22 18:04:49 +0700

    impl backward propagation for relu unit

--- models/ReluLayer.py
+++ models/ReluLayer.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from ActivationFunctions import relu
 from LayerBase import LayerBase
 
@@ -7,3 +9,11 @@ class ReluLayer(LayerBase):
         super(ReluLayer, self).forward(size_of_previous_layer)
         self.activation_layer = relu(self.pre_activation)
         return self.activation_layer
+
+    def backward(self, d_activation):
+        d_pre_activation = np.array(d_activation, copy=True)
+        d_pre_activation[self.pre_activation <= 0] = 0
+
+        super(ReluLayer, self).backward(d_pre_activation)
+
+        return self.d_activation
