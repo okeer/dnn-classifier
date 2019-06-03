b1f3840756c11113f77b26b0d2adf765b947af28
Sergey Orlov <serhio.orlovsky@gmail.com>
2019-06-04 00:31:47 +0700

    dnn: implement mini-batch processing

--- dnnclassifier/NeuralNetwork.py
+++ dnnclassifier/NeuralNetwork.py
@@ -16,15 +16,30 @@ class NeuralNetwork(object):
             self.propagation_features = self.layers[index].forward(self.propagation_features)
 
     def __backward_propagation(self, classes):
-        d_activation = - (np.divide(classes, self.propagation_features) - np.divide(1 - classes, 1 - self.propagation_features))
+        d_activation = - (np.divide(classes, self.propagation_features) - np.divide(1 - classes,
+                                                                                    1 - self.propagation_features))
         for index in reversed(range(len(self.layers))):
             d_activation = self.layers[index].backward(d_activation)
             self.layers[index].update(self.learning_rate)
 
-    def train(self, features, classes):
-        for index in range(self.iterations):
-            self.__forward_propagation(features)
-            self.__backward_propagation(classes)
+    def train(self, features, classes, chunk_size=None):
+        if chunk_size is not None and chunk_size > classes.shape[1]:
+            raise Exception("Chunk size should be less than number of examples")
+        if chunk_size is None:
+            chunk_size = classes.shape[1]
+
+        chunks = np.round(features.shape[1] / chunk_size).astype(int)
+        split_indices = np.arange(start=chunk_size, step=chunk_size, stop=chunk_size * chunks)
+
+        for epoch in range(self.iterations):
+            rand_perm = np.random.permutation(features.shape[1])
+
+            X_chunks = np.hsplit(features[:, rand_perm], split_indices)
+            Y_chunks = np.hsplit(classes[:, rand_perm], split_indices)
+
+            for index in range(len(X_chunks)):
+                self.__forward_propagation(X_chunks[index])
+                self.__backward_propagation(Y_chunks[index])
 
     def predict(self, features):
         self.__forward_propagation(features)
